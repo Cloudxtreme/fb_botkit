83ee62ea4c1ecf5d135e0909874fcc7e4e907a69
Neetu  Mishra <1191193@India.TCS.com>
2017-03-03 13:26:34 +0530

    updated wid lib

--- facebook_bot.js
+++ facebook_bot.js
@@ -1,19 +1,4 @@
-if (!process.env.page_token) {
-    console.log('Error: Specify page_token in environment');
-    process.exit(1);
-}
-
-if (!process.env.verify_token) {
-    console.log('Error: Specify verify_token in environment');
-    process.exit(1);
-}
-
-if (!process.env.app_secret) {
-    console.log('Error: Specify app_secret in environment');
-    process.exit(1);
-}
-
-var Botkit = require('botkit');
+var Botkit = require('./lib/Botkit.js');
 
 
 var controller = Botkit.facebookbot({
@@ -25,28 +10,18 @@ var controller = Botkit.facebookbot({
     validate_requests: true, // Refuse any requests that don't come from FB on your receive webhook, must provide FB_APP_SECRET in environment variables
 });
 
+
 var bot = controller.spawn({
 });
 
-controller.hears(['quick'], 'message_received', function(bot, message) {
 
-    bot.reply(message, {
-        text: 'Hey! This message has some quick replies attached.',
-        quick_replies: [
-            {
-                "content_type": "text",
-                "title": "Yes",
-                "payload": "yes",
-            },
-            {
-                "content_type": "text",
-                "title": "No",
-                "payload": "no",
-            }
-        ]
+controller.setupWebserver(process.env.PORT || process.env.port || 5000), function(err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function() {
     });
+};
 
-});
+
+controller.api.thread_settings.greeting('Hello! I\'m a Botkit bot!');
 
 controller.hears(['^hello', '^hi'], 'message_received,facebook_postback', function(bot, message) {
     bot.reply(message, 'Hello iam bot.');
