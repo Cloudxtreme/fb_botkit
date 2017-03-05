b807125bcb1f8d092377dca41da0e9dfee5e2a8f
neetucompiler <neetumishra231093@gmail.com>
2017-03-05 13:41:07 +0530

    Update facebook_bot.js

--- facebook_bot.js
+++ facebook_bot.js
@@ -159,6 +159,7 @@ controller.hears(['where','location','located','^where','^location'], 'message_r
 			{
                                 pattern: 'yes',
                                 callback: function(response, convo) {
+				     convo.say('then we can meet up in the event then.See you soon!');
                                     // since no further messages are queued after this,
                                     // the conversation will end naturally with status == 'completed'
                                     convo.next();
@@ -167,6 +168,7 @@ controller.hears(['where','location','located','^where','^location'], 'message_r
                             {
                                 pattern: 'no',
                                 callback: function(response, convo) {
+				     convo.say('So i can help you with that!');
                                     // stop the conversation. this will cause it to end with status == 'stopped'
                                     convo.stop();
                                 }
