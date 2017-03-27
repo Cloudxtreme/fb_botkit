bce70505626618c4a19ff4e0cf37752036c54aff
neetucompiler <neetumishra231093@gmail.com>
2017-03-27 11:47:32 +0530

    greeting changed

--- facebook_bot.js
+++ facebook_bot.js
@@ -20,7 +20,8 @@ controller.setupWebserver(process.env.PORT || process.env.port || 3000, function
 });
 
 controller.api.thread_settings.greeting('Welcome to ATUNE Event. I am ATUNE Bot to provide event details');
-controller.api.thread_settings.get_started('Get Started');
+controller.api.thread_settings.get_started();
+
 
 controller.api.thread_settings.menu([
     {
