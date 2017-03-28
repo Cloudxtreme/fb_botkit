33a2bdb308f6ce12ff8d6fac643913466d3a0eb1
neetucompiler <neetumishra231093@gmail.com>
2017-03-28 13:05:56 +0530

    Update facebook_bot.js

--- facebook_bot.js
+++ facebook_bot.js
@@ -492,6 +492,23 @@ controller.hears(['Sight Seeing'], 'message_received,facebook_postback', functio
 controller.hears(['Travel'], 'message_received,facebook_postback', function (bot, message) {
    bot.startConversation(message, function(err, convo) {
                 convo.say('For Travel Details you can contact Mr.XYZABCD , Ph no: 9999955555');
+        convo.ask({
+              "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"What do you want to do next?",
+                "buttons":[
+                  {
+                    "type":"postback",
+                    "title":"Go Back",
+                    "payload":"Event Organizers"
+                  }
+                ]
+              }
+            }
+        });
+    convo.next();
     });
 });
 
