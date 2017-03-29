67966a0c46a197ac32c094297756ccdd57aa7c6b
Neetu  Mishra <1191193@India.TCS.com>
2017-03-29 17:25:52 +0530

    commited with initial1 details

--- facebook_bot.js
+++ facebook_bot.js
@@ -523,47 +523,26 @@ controller.hears(['Agenda','agenda'], 'message_received,facebook_postback', func
           convo.say('Day 0 : 22-Apr-2017: 5:00 PM onwards to 9:30 PM');
           convo.say('Day 0 : 22-Apr-2017: 5:00 PM onwards to 9:30 PM');  
           convo.say('I can share further details once I get them. The Organizers are finalising as we speak');
-           convo.ask({
+            convo.ask({
               "attachment":{
               "type":"template",
               "payload":{
                 "template_type":"button",
-                "text":"What would you like to know about next?",
+                "text":"What do you want to do next?",
                 "buttons":[
                   {
                     "type":"postback",
                     "title":"Go Back",
                     "payload":"Conference Details"
+                  },{
+                    "type":"postback",
+                    "title":"Main Menu",
+                    "payload":"start_payload"
                   }
                 ]
               }
             }
         });
-         convo.ask({
-              "attachment":{
-              "type":"template",
-              "payload":{
-                "template_type":"button",
-                "text":"What would you like to know about next?",
-                "buttons":[
-                  {
-                    "type":"postback",
-                    "title":"Agenda",
-                    "payload":"Agenda"
-                  },{
-                    "type":"postback",
-                    "title":"Location Details",
-                    "payload":"Location Details"
-                  },{
-                    "type":"postback",
-                    "title":"Other Details",
-                    "payload":"Other Details"
-                  }
-                ]
-              }
-            }
-        });
-    convo.next();
     }); 
 });
 
